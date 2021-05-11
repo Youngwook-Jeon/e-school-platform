2c9890210a0adbaaa8f73baee6f982af7fe54e91
Young wook Jeon <ela87@naver.com>
2021-05-11 22:16:08 +0900

    Let our server ready

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log("server setup");
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+const morgan = require("morgan");
+require("dotenv").config();
+
+// create express app
+const app = express();
+
+// apply middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// route
+fs.readdirSync("./routes").map((r) =>
+  app.use("/api", require(`./routes/${r}`))
+);
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
