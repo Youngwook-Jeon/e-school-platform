838593c9c1ece337e1d4123e80be9fdf4c7777fe
Young wook Jeon <ela87@naver.com>
2021-05-17 22:04:29 +0900

    Add page for becoming instructor

---
+++ client/pages/instructor/course/create.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const CreateCourse = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center square">강의 만들기</h1>
+    </>
+  );
+};
+
+export default CreateCourse;
