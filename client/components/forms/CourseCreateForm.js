eda7237249eaca8c264fe56c2183edc60cc04b7a
Young wook Jeon <ela87@naver.com>
2021-05-18 21:41:29 +0900

    Add a component for creating course

---
+++ client/components/forms/CourseCreateForm.js
@@ -0,0 +1,123 @@
+import { Select, Button, Avatar } from "antd";
+
+const { Option } = Select;
+
+const CourseCreateForm = ({
+  handleSubmit,
+  handleImage,
+  handleChange,
+  values,
+  setValues,
+  preview,
+}) => {
+  const children = [];
+  for (let i = 10000; i <= 100000; i += 10000) {
+    children.push(<Option key={i}>{i}원</Option>);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="강의 제목"
+          value={values.name}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <textarea
+          name="description"
+          cols="7"
+          rows="7"
+          value={values.description}
+          className="form-control"
+          onChange={handleChange}
+          placeholder="강의에 대한 간략한 설명"
+        ></textarea>
+      </div>
+
+      <div className="form-row">
+        <div className="col">
+          <div className="form-group">
+            <Select
+              style={{ width: "100%" }}
+              size="large"
+              value={values.paid}
+              onChange={(v) => setValues({ ...values, paid: !values.paid })}
+            >
+              <Option value={true}>유료</Option>
+              <Option value={false}>무료</Option>
+            </Select>
+          </div>
+        </div>
+
+        {values.paid && (
+          <div className="form-group">
+            <Select
+              defaultValue="10000원"
+              style={{ width: "100%" }}
+              onChange={(v) => setValues({ ...values, price: v })}
+              tokenSeparators={[,]}
+              size="large"
+            >
+              {children}
+            </Select>
+          </div>
+        )}
+      </div>
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="category"
+          className="form-control"
+          placeholder="카테고리"
+          value={values.category}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-row">
+        <div className="col">
+          <div className="form-group">
+            <label className="btn btn-outline-secondary w-100 text-left">
+              {values.loading ? "업로딩..." : "이미지 업로드"}
+
+              <input
+                type="file"
+                name="image"
+                onChange={handleImage}
+                accept="image/*"
+                hidden
+              />
+            </label>
+          </div>
+        </div>
+
+        {preview && <Avatar width={200} src={preview} />}
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <button
+            onClick={handleSubmit}
+            disabled={values.loading || values.uploading}
+            className="btn btn-primary"
+            loading={values.loading}
+            type="primary"
+            size="large"
+            shape="round"
+          >
+            {values.loading ? "저장중..." : "저장하고 계속하기"}
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CourseCreateForm;
--- client/pages/instructor/course/create.js
+++ client/pages/instructor/course/create.js
@@ -1,10 +1,47 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 import InstructorRoute from "../../../components/routes/InstructorRoute";
+import CourseCreateForm from "../../../components/forms/CourseCreateForm";
 
 const CreateCourse = () => {
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "9.99",
+    uploading: false,
+    paid: true,
+    category: "",
+    loading: false,
+  });
+
+  const [preview, setPreview] = useState("");
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleImage = (e) => {
+    setPreview(window.URL.createObjectURL(e.target.files[0]))
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <InstructorRoute>
       <h1 className="jumbotron text-center square">강의 만들기</h1>
+
+      <div className="pt-3 pb-3">
+        <CourseCreateForm
+          handleSubmit={handleSubmit}
+          handleImage={handleImage}
+          handleChange={handleChange}
+          values={values}
+          setValues={setValues}
+          preview={preview}
+        />
+      </div>
     </InstructorRoute>
   );
 };
