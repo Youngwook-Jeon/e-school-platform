8ec472596fb130721a742a13ea8f634821ef1384
Young wook Jeon <ela87@naver.com>
2021-05-26 14:04:18 +0900

    Add icons conditionally

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -3,7 +3,13 @@ import { useRouter } from "next/router";
 import InstructorRoute from "../../../../components/routes/InstructorRoute";
 import axios from "axios";
 import { Avatar, Tooltip, Button, Modal, List } from "antd";
-import { EditOutlined, CheckOutlined, UploadOutlined } from "@ant-design/icons";
+import {
+  EditOutlined,
+  CheckOutlined,
+  UploadOutlined,
+  QuestionOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
 import ReactMarkdown from "react-markdown";
 import AddLessonForm from "../../../../components/forms/AddLessonForm";
 import { toast } from "react-toastify";
@@ -93,6 +99,14 @@ const CourseView = () => {
     }
   };
 
+  const handlePublish = (e, courseId) => {
+
+  }
+
+  const handleUnpublish = (e, courseId) => {
+    
+  }
+
   return (
     <InstructorRoute>
       <div className="container-fluid pt-3">
@@ -127,9 +141,19 @@ const CourseView = () => {
                       />
                     </Tooltip>
 
-                    <Tooltip title="Publish">
-                      <CheckOutlined className="h5 pointer text-danger" />
-                    </Tooltip>
+                    {course.lessons && course.lessons.length < 5 ? (
+                      <Tooltip title="퍼블리시하려면 적어도 5개의 강의가 필요합니다.">
+                        <QuestionOutlined className="h5 pointer text-danger" />
+                      </Tooltip>
+                    ) : course.published ? (
+                      <Tooltip title="Unpublish">
+                        <CloseOutlined onClick={e => handleUnpublish(e, course._id)} className="h5 pointer text-danger" />
+                      </Tooltip>
+                    ) : (
+                      <Tooltip title="Publish">
+                        <CheckOutlined onClick={e => handlePublish(e, course._id)} className="h5 pointer text-success" />
+                      </Tooltip>
+                    )}
                   </div>
                 </div>
               </div>
