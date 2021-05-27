f0b848d3b00fa5b89834ca24a34ad4c75d2e6637
Young wook Jeon <ela87@naver.com>
2021-05-28 01:37:45 +0900

    Add enroll button

--- client/pages/course/[slug].js
+++ client/pages/course/[slug].js
@@ -1,15 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import SingleCourseJumbotron from "../../components/cards/SingleCourseJumbotron";
 import PreviewModal from "../../components/modal/PreviewModal";
 import SingleCourseLessons from "../../components/cards/SingleCourseLessons";
+import { Context } from "../../context";
 
 const SingleCourse = ({ course }) => {
   // const router = useRouter();
   // const { slug } = router.query;
   const [showModal, setShowModal] = useState(false);
   const [preview, setPreview] = useState("");
+  const [loading, setLoading] = useState(false);
+  const {
+    state: { user },
+  } = useContext(Context);
+
+  const handlePaidEnrollment = () => {};
+
+  const handleFreeEnrollment = () => {};
 
   return (
     <>
@@ -19,6 +28,10 @@ const SingleCourse = ({ course }) => {
         setShowModal={setShowModal}
         preview={preview}
         setPreview={setPreview}
+        user={user}
+        loading={loading}
+        handlePaidEnrollment={handlePaidEnrollment}
+        handleFreeEnrollment={handleFreeEnrollment}
       />
 
       <PreviewModal
