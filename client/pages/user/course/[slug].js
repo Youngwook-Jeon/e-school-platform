39f2628b704947a199e9557e36401cf088d0f5ba
Young wook Jeon <ela87@naver.com>
2021-06-02 00:15:14 +0900

    Add a middleware to check whether or not a user enrolled a course

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -1,12 +1,27 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
+import StudentRoute from "../../../components/routes/StudentRoute";
 
 const SingleCourse = () => {
+  const [loading, setLoading] = useState(false);
+  const [course, setCourse] = useState({ lessons: [] });
   const router = useRouter();
+  const { slug } = router.query;
+
+  useEffect(() => {
+    if (slug) loadCourse();
+  }, [slug]);
+
+  const loadCourse = async () => {
+    const { data } = await axios.get(`/api/user/course/${slug}`);
+    setCourse(data);
+  };
 
   return (
-    <>
-      <h1>Course slug is: {JSON.stringify(router.query.slug)}</h1>
-    </>
+    <StudentRoute>
+      <h1>Course slug is: {JSON.stringify(course)}</h1>
+    </StudentRoute>
   );
 };
 
