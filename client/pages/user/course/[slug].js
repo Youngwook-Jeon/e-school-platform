05330a0681f7c537b00eb48ccaad698893c76720
Young wook Jeon <ela87@naver.com>
2021-06-02 16:29:57 +0900

    Show clicked lesson contents

--- client/pages/user/course/[slug].js
+++ client/pages/user/course/[slug].js
@@ -1,9 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createElement } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import StudentRoute from "../../../components/routes/StudentRoute";
+import { Button, Menu, Avatar } from "antd";
+import ReactPlayer from "react-player";
+import ReactMarkdown from "react-markdown";
+import {
+  PlayCircleOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from "@ant-design/icons";
+
+const { Item } = Menu;
 
 const SingleCourse = () => {
+  const [clicked, setClicked] = useState(-1);
+  const [collapsed, setCollapsed] = useState(false);
   const [loading, setLoading] = useState(false);
   const [course, setCourse] = useState({ lessons: [] });
   const router = useRouter();
@@ -20,7 +32,65 @@ const SingleCourse = () => {
 
   return (
     <StudentRoute>
-      <h1>Course slug is: {JSON.stringify(course)}</h1>
+      <div className="row">
+        <div style={{ maxWidth: 320 }}>
+          <Button
+            onClick={() => setCollapsed(!collapsed)}
+            className="text-primary mt-1 w-100 mb-2"
+          >
+            {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}{" "}
+            {!collapsed && "강의"}
+          </Button>
+          <Menu
+            defaultSelectedKeys={[clicked]}
+            inlineCollapsed={collapsed}
+            style={{ height: "80vh", overflow: "scroll" }}
+          >
+            {course.lessons.map((lesson, index) => (
+              <Item
+                onClick={() => setClicked(index)}
+                key={index}
+                icon={<Avatar>{index + 1}</Avatar>}
+              >
+                {lesson.title.substring(0, 30)}
+              </Item>
+            ))}
+          </Menu>
+        </div>
+
+        <div className="col">
+          {clicked !== -1 ? (
+            <>
+              {course.lessons[clicked].video &&
+                course.lessons[clicked].video.Location && (
+                  <>
+                    <div className="wrapper">
+                      <ReactPlayer
+                        className="player"
+                        url={course.lessons[clicked].video.Location}
+                        width="100%"
+                        height="100%"
+                        controls
+                      />
+                    </div>
+                  </>
+                )}
+
+              <ReactMarkdown
+                children={course.lessons[clicked].content}
+                className="single-post"
+              />
+            </>
+          ) : (
+            <div className="d-flex justify-content-center p-5">
+              <div className="text-center p-5">
+                <PlayCircleOutlined className="text-primary display-1 p-5" />
+                <p className="lead">수강하려는 강의를 클릭하세요.</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </StudentRoute>
   );
 };
